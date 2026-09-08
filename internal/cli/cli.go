9d84da44622e442ebf1a7f26db2a49632886d7c8
mycodeself <ismael.pineiro@lansweeper.com>
2026-09-08 13:04:53 +0200

    chore(ci): set tagalign.align to true in golangci-lint
    Signed-off-by: mycodeself <ismael.pineiro@lansweeper.com>

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -18,12 +18,12 @@ type Credentials struct {
 
 // Data holds all the possible values feeding the application.
 type Data struct {
-	APIVersions      []string          `default:""                            env:"KUBE_API_VERSIONS"   help:"API versions (capabilities)." short:"a"`
-	Files            []string          `help:"Values files, relative to Path (or to --repo-root if absolute)."        short:"f"             type:"string"`
-	KubeVersion      string            `default:""                            env:"KUBE_VERSION"        help:"Kubernetes version."          short:"k"`
-	ReleaseName      string            `env:"ARGOCD_APP_NAME"                 help:"Release name."      short:"r"`
-	ReleaseNamespace string            `env:"ARGOCD_APP_NAMESPACE"            help:"Release namespace." short:"n"`
-	Set              map[string]string `help:"Injected key value pairs."      mapsep:","                short:"v"`
+	APIVersions      []string          `default:""                                                             env:"KUBE_API_VERSIONS"   help:"API versions (capabilities)." short:"a"`
+	Files            []string          `help:"Values files, relative to Path (or to --repo-root if absolute)." short:"f"                 type:"string"`
+	KubeVersion      string            `default:""                                                             env:"KUBE_VERSION"        help:"Kubernetes version."          short:"k"`
+	ReleaseName      string            `env:"ARGOCD_APP_NAME"                                                  help:"Release name."      short:"r"`
+	ReleaseNamespace string            `env:"ARGOCD_APP_NAMESPACE"                                             help:"Release namespace." short:"n"`
+	Set              map[string]string `help:"Injected key value pairs."                                       mapsep:","                short:"v"`
 }
 
 // Logging holds the logging configuration for the application.
